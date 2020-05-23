8d1a0422b1e74da01432935a514fcbac60b1845e
Sophakneath <pheakneath8@gmail.com>
2020-05-23 12:53:15 +0700

    push project

---
+++ vueAPp/src/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <Header/>
+      <router-view/>
+  </div>
+</template>
+
+<script>
+
+import Header from './layout/Header';
+export default {
+  name:"app",
+  components:{
+    Header
+  }
+}
+</script>
+
+<!-- Global CSS -->
+<style>
+  .btn{
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover{
+    background: #666;
+  }
+</style>
---
+++ vuex/src/components/Todos.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+        <h3>Todos</h3>
+
+        <div class="legend">
+            <span>Double click to mark as complete</span>
+            <span>
+                <span class="incomplete-box"/> = Incomplete
+            </span>
+            <span>
+                <span class="complete-box"/> = Complete
+            </span>
+        </div>
+
+        <div class="todos">
+            <div @click="onDblClick(todo)" class="todo" v-for="todo in allTodos" v-bind:key="todo.id" v-bind:class="{'is-complete':todo.completed}">
+                {{todo.title}}
+                <h6 @click="deleteTodo(todo.id)">X</h6>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+    name: "Todos",
+    methods:{
+        ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']),
+        onDblClick(todo){
+
+            console.log("hello");
+
+            const updTodo = {
+                id: todo.id,
+                title: todo.title,
+                completed: !todo.completed
+            };
+
+            this.updateTodo(updTodo);
+        }
+    },
+    computed: mapGetters(['allTodos']),
+    created(){
+        this.fetchTodos();
+    }
+};
+</script>
+
+<style scoped>
+    .todos{
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
+        grid-gap: 1rem;
+    }
+
+    .todo{
+        border: 1px solid #ccc;
+        background: #41b883;
+        padding: 1rem;
+        border-radius: 5px;
+        text-align: center;
+        position: relative;
+        cursor: pointer;
+    }
+
+    h6{
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
+        color: #fff;
+        cursor: pointer;
+    }
+    .legend
+    {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 1rem;
+    }
+
+    .complete-box{
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: #35495e;
+    }
+
+    .incomplete-box{
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: #41b833;
+    }
+    @media (max-width: 500px){
+        .todos{
+            grid-template-columns: 1fr;
+        }
+    }
+
+    .is-complete{
+        background: #35495e;
+        color: #fff;
+    }
+</style>
